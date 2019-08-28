780aba0011f48ac654a2c180106fe9db039fd4ce
Harry Nicholls <harry.m.nicholls@gmail.com>
2019-08-28 10:02:14 -0400

    Moves login function to background.js

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+import { login } from './utils';
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
@@ -10,5 +12,20 @@ chrome.runtime.onInstalled.addListener(function() {
         actions: [new chrome.declarativeContent.ShowPageAction()],
       },
     ]);
+  });
+});
+
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name == 'login');
+  port.onMessage.addListener(function(msg) {
+    console.log(msg);
+    if (msg.action == 'login') {
+      login()
+        .then(token => {
+          console.log('Setting token...', token);
+          port.postMessage({ token });
+        })
+        .catch(error => console.error(error));
+    }
   });
 });
